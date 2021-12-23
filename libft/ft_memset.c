5ce6302e48f670af9e987b35454ea3eafffbf2b7
Kazuki Mori <kamori@c1r20s10.42tokyo.jp>
2021-12-23 23:49:11 +0900

    mememe

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: kamori <kamori@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/25 01:36:49 by kamori            #+#    #+#             */
-/*   Updated: 2020/11/26 10:51:08 by kamori           ###   ########.fr       */
+/*   Updated: 2021/12/23 23:48:01 by kamori           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,9 @@
 
 void	*ft_memset(void *b, int c, size_t len)
 {
-	int				i;
 	unsigned char	*censored;
 
 	censored = (unsigned char *)b;
-	i = 0;
 	while (len > 0)
 	{
 		*censored = (unsigned char)c;
